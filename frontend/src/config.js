c2a2a9a7add6339363bfe77397702ca404e931e1
Huo Yingzhe <z5405325@ad.unsw.edu.au>
2024-06-27 01:56:19 +1000

    fix change backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://localhost:5001',
+    backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://127.0.0.1:5001',
   };
   
   export default config;
