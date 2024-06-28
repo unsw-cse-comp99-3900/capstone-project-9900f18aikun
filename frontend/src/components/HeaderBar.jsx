a629d0359579b3130a398e9236d5ccd2839cd982
Ziwen Zheng <z5499025@ad.unsw.edu.au>
2024-06-28 14:49:34 +1000

    beautify the logout btn

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -7,7 +7,10 @@ const HeaderBar = ({ onLogout }) => {
       <div className="overlap">
         <img className="image" alt="UNSW Logo" src="/img/image-164.png" />
         {/* <div className="text-wrapper-2">History</div> */}
-        <button className="logout-button" onClick={onLogout}>Logout</button>
+        <div className="button-group">
+          {/* 这里可以添加其他按钮 */}
+          <button className="logout-button">Logout</button>
+        </div>
       </div>
     </div>
   );
