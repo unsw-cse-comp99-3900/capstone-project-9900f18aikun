008adb4a92ce9e5fef2f8ac9a9e3b7d3419b3bfe
Ziwen Zheng <z5499025@ad.unsw.edu.au>
2024-07-16 15:06:22 +1000

    add admin home button

--- frontend/src/components/admin_comp/adminHome.js
+++ frontend/src/components/admin_comp/adminHome.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Popover, Button, Tooltip } from '@arco-design/web-react';
+import './adminHome.css'; // 引入CSS文件
 
 function AdminHome() {
+    const colors = ['#FCE996', '#C396ED', '#BEDAFF'];
+    const buttonLabels = ['Appointment numbers', 'Classroom numbers', 'Student numbers'];
+
     return (
         <div>
             <h1>Welcome !</h1>
+            <div className="button-container"> 
+                {colors.map((color, index) => {
+                    return (
+                        <Tooltip key={color} color={color} content='tooltip text'>
+                            <Button
+                                className="button-style" // 应用按钮样式
+                                style={{
+                                    background: color, // 保留背景颜色的内联样式
+                                }}
+                            >
+                                {buttonLabels[index]}
+                            </Button>
+                        </Tooltip>
+                    );
+                })}
+            </div>
         </div>
     );
 }
