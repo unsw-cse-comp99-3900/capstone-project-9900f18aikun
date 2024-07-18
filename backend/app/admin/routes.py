7b90b3af07d12fb97fcd1261b1814dddc81a66c1
wwy6191 <804404802@qq.com>
2024-07-18 13:33:07 +1000

    update report

--- backend/app/admin/routes.py
+++ backend/app/admin/routes.py
@@ -5,7 +5,7 @@ from app.extensions import db, api
 from app.models import Users, CSEStaff
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, verify_jwt_in_request
 from app.utils import start_end_time_convert
-from app.email import schedule_reminder, send_confirm_email_async
+from app.email import get_email, schedule_reminder, send_confirm_email_async, send_report_email_async
 from jwt import exceptions
 from app.booking.models import Booking
 from app.utils import verify_jwt, is_admin
@@ -14,6 +14,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from app.utils import is_admin, get_user_name
 
 admin_ns = Namespace('admin', description='Admin operations')
+
 @admin_ns.route("/check_admin")
 class CheckAdmin(Resource):
     @admin_ns.doc(description="Check user whether is admin")
@@ -32,3 +33,31 @@ class CheckAdmin(Resource):
             return {'error': "can't get admin"}, 403
         return {"is_admin": whether_admin}, 200
 
+
+report_model = admin_ns.model('send report', {
+    'message': fields.String(required=True, description='report content', default="G01 table missing")
+})
+
+@admin_ns.route("/report")
+class report(Resource):
+    @admin_ns.doc(description="send report to admin")
+    @admin_ns.response(200, "Success")
+    @admin_ns.response(400, "Bad request")
+    @admin_ns.expect(report_model)
+    @api.header('Authorization', 'Bearer <your_access_token>', required=True)
+    def post(self):
+        jwt_error = verify_jwt()
+        if jwt_error:
+            return jwt_error
+        msg = request.json["message"]
+        current_user = get_jwt_identity()
+        user_zid = current_user['zid']
+        to_zid = "z5"
+        to_name = get_user_name(to_zid)
+        send_report_email_async(user_zid, to_zid, msg)
+        return {
+            "reporter": f"{user_zid}",
+            "message": f"{msg}",
+            "to_zid": f"{to_zid}",
+            "to_name": f"{to_name}"
+        }, 200
